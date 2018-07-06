d9e2c6cf47b486b839b821ac0ebcca048864e0db
Jorge Guerrero <jorgegro1999@gmail.com>
2018-07-05 21:01:53 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  name = 'Jorge';
+  todoInput = '';
+
+  array = [];
+
+  myFunction(chore) {
+    console.log(chore)
+  }
+
+  editTodo(chore) {
+    let index = this.array.indexOf(chore);
+
+    this.array[index] = prompt('edit!')
+  }
+
+  addTodo(chore){
+    console.log('sdfjadlk');
+    this.array.push(this.todoInput);
+    this.todoInput = '';
+  }
 }
